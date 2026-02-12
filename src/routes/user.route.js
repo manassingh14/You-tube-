fad5eac02e64e83161a7a584a9c29b7580eb1c82
manassingh14 <manojsinghh196999@gmail.com>
2026-02-13 02:03:54 +0530

    Add user account routes for password change, current user retrieval, and media updates

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { userLoginHandler, userRegistrationHandler,userLogout,refresAccessToken } from "../controllers/user.controller.js";
+import { userLoginHandler, userRegistrationHandler,userLogout,refresAccessToken, changeCurrentPassword, getCurrentUser, upadteAccount,avatarUpdate, coverImageUpdate, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/mullter.middleware.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
@@ -8,5 +8,12 @@ userRouter.post("/register", upload.fields([
 ]), userRegistrationHandler);
 userRouter.post("/login",userLoginHandler);
 userRouter.post("/logout",verifyjwt,userLogout);
-userRouter.post("/refres-Token",refresAccessToken)
+userRouter.post("/refres-Token",refresAccessToken);
+userRouter.post("/change-password",verifyjwt,changeCurrentPassword);
+userRouter.get("/current-user",verifyjwt,getCurrentUser);
+userRouter.patch("/update-account",verifyjwt,upadteAccount);
+userRouter.patch("/update-avatar",verifyjwt,upload.single("avatar"),avatarUpdate);
+userRouter.patch("/cover-image",verifyjwt,upload.single("/coverImage"),coverImageUpdate);
+userRouter.get("/channel/:username",verifyjwt,getUserChannelProfile);
+userRouter.get("/watch-history",verifyjwt,getWatchHistory)
 export default userRouter;
